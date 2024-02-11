371bdfd455bee1c5e95d1ea18fe0aed5c279ce77
sklimasz <sallonator@gmail.com>
2024-02-12 00:02:56 +0100

    Rewrite one poor function.

--- utils.py
+++ utils.py
@@ -29,33 +29,32 @@ def prepare_champion_data(champions_data, level_data, pool_data):
 
 def apply_pool_depletion(champions_data, simulation_data):
     headliners = simulation_data["headliners"]
-    headliners_names = [headliner["name"] for headliner in headliners]
+    hd_names = {headliner["name"] for headliner in headliners}
+    other = simulation_data.get("other", [])
+    other_names = {data.get("name") for data in other}
+
     for headliner in headliners:
         for champion in champions_data:
             if headliner["name"] == champion["name"]:
                 champion["copies_taken"] = headliner.get("copies_taken", 0)
                 break
-    
-    other = simulation_data.get("other", None)
 
-    # Rewrite this disgusting code below.
-    # FIXME
-    if other is not None:
-        other_names = [elem.get("name", None) for elem in other]
-        for data in other:
-            if (name := data.get("name")) is not None:
-                # If champion name is specified, apply it a champion
-                # but not if the champion was mentioned in 'headliners' category
-                # You can apply copies taken directly in the headliner category.
-                for champion in champions_data:
-                    if champion["name"] not in headliners_names and name == champion["name"]:
-                        champion["copies_taken"] = data["copies_taken"]
-                        break
-            if (cost := data.get("cost")) is not None:
-                # If cost is specified, apply it to all champions of the cost
-                # but not to the champions mentioned in 'headliners' and 'other' category
-                for champion in champions_data:
-                    if champion["name"] not in headliners_names+other_names and cost == champion["cost"]:
-                        champion["copies_taken"] = data["copies_taken"]
+    for data in other:
+        name = data.get("name")
+        cost = data.get("cost")
+
+        for champion in champions_data:
+            if name == champion["name"] and name not in hd_names:
+            # Don't specify headliners copies taken in 'other' category
+            # Do it directly in the 'headliners' category
+                champion["copies_taken"] = data["copies_taken"]
+                break
+
+            if champion["cost"] == cost and champion["name"] not in hd_names|other_names:
+            # Update copies_taken of all champions with the given cost,
+            # but not if they were mentioned in 'other' or 'headliners' categories.
+            # copies_taken of overlapping champions won't be changed twice.
+                champion["copies_taken"] = data["copies_taken"]
+
     return champions_data
 

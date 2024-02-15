4a5ceaf842357e1a017f7811eda1adda7b1090f0
sklimasz <sallonator@gmail.com>
2024-02-15 21:12:03 +0100

    Rewrite run_rolldown and utils to be easily testable.

--- roll_sim/scripts/run_rolldown.py
+++ roll_sim/scripts/run_rolldown.py
@@ -2,7 +2,10 @@ from pathlib import Path
 
 import click
 
-from ..code import champion, roll, statistics, utils
+from roll_sim.code import utils
+from roll_sim.code.champion import Champion
+from roll_sim.code.roll import RolldownSimulator
+from roll_sim.code.statistics import get_stats
 
 
 @click.command()
@@ -14,48 +17,68 @@ def cli(conf, rolldowns, rules, stats):
     # Load config
     conf = utils.load_json(conf)
 
-    # Sanity checks and sweet walruses
-    if (level := conf.get("level")) is None:
-        raise ValueError("No level data in conf")
-    if (headliners := conf.get("headliners")) is None:
-        raise ValueError("No headliners data in conf")
+    # Get and validate data from config
+    headliners, level, other = get_and_validate_data(conf = conf)
 
     # Echo conf data
     click.echo(f"\nRequested headliners at {level=}:")
     for headliner in headliners:
         click.echo(headliner)
 
-    # Prepare data
-    path = Path(__file__).parent.resolve()
-    path_parent = path.parent.resolve()
-    champions_data = utils.load_json(path_parent / "data/champions.json")
-    level_data = utils.load_yaml(path_parent / "data/levels.yml")
-    level_data = utils.get_level_odds(level = level, level_data=level_data )
-    pool_data = utils.load_yaml(path_parent / "data/champion_pool_sizes.yml")
+    # Ensure that this gets data from data folder correctly,
+    # wherever it is run from.
+    path_script = Path(__file__).parent.resolve()
+    path_roll_sim = path_script.parent.resolve()
+    path_data = path_roll_sim / "data"
 
-    champions = utils.prepare_champion_data(champions_data, level_data, pool_data)
-    champions = utils.apply_pool_depletion(champions, conf)
-    champions = champion.Champion.from_list(champions)
-    headliners = champion.Champion.from_list(headliners)
+    # Load and prepare data, apply pool depletion, convert into Champion objects.
+    champions, headliners = prepare_data(path_data, headliners, level, other)
 
-    # Simulate rolldown
-    simulator = roll.RolldownSimulator(champions=champions, headliners_to_buy=headliners)
+    # Simulate rolldown and get results.
+    simulator = RolldownSimulator(champions=champions, headliners_to_buy=headliners)
     avg_rolls = simulator.roll(rolldowns=rolldowns, bad_luck_rules=rules)
 
-    # Display results
+    # Display results.
     click.echo("\nRolldown results:")
     click.echo(f"average rolls = {round(avg_rolls, 2)}")
 
-    # Compute and display stats
+    # Compute and display stats.
     if stats:
-        rolls = simulator.rolls_list
+        rolls_list = simulator.rolls_list
         probabilities = [0.5, 0.75, 0.9]
-        rolls_needed_list, median, stdev = statistics.get_stats(rolls, probabilities)
+        rolls_needed_list, median, stdev = get_stats(rolls_list, probabilities)
         click.echo(f"median = {round(median, 2)}")
         click.echo(f"deviation  = {round(stdev, 2)}\n")
         click.echo("Rolls needed to achieve probability:")
         for rolls_needed, prob in zip(rolls_needed_list, probabilities):
             click.echo(f"{rolls_needed} rolls -> {prob*100}%")
+
+def get_and_validate_data(conf: dict) -> tuple[dict, dict, dict]:
+    """Get data from config and ensure it contains necessary data."""
+    if (level := conf.get("level")) is None:
+        raise ValueError("No level data in conf")
+
+    if (headliners := conf.get("headliners")) is None:
+        raise ValueError("No headliners data in conf")
+
+    other = conf.get("other")
+    return headliners, level, other
+
+def prepare_data(data_path: Path,
+                headliners: dict,
+                level: int,
+                other: dict | None) -> tuple[list[Champion], list[Champion]]:
+    """Load and prepare data, apply pool depletion, convert into Champion objects."""
+    champions_data = utils.load_json(data_path / "champions.json")
+    level_data = utils.load_yaml(data_path / "levels.yml")
+    level_data = utils.get_level_odds(level = level, level_data=level_data )
+    pool_data = utils.load_yaml(data_path / "champion_pool_sizes.yml")
+
+    champions = utils.prepare_champion_data(champions_data, level_data, pool_data)
+    champions = utils.apply_pool_depletion(champions, headliners, other)
+    champions = Champion.from_list(champions)
+    headliners = Champion.from_list(headliners)
+    return champions, headliners
 
 if __name__ == "__main__":
     cli()

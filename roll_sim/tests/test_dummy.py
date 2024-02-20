4fc2195b6424530457eee0b970fdcb12928068f5
sklimasz <sallonator@gmail.com>
2024-02-20 20:00:18 +0100

    cov fix and test

--- roll_sim/tests/test_dummy.py
+++ roll_sim/tests/test_dummy.py
@@ -5,3 +5,15 @@ def test_dummu1():
 
 def test_dummy2():
     assert 0 == 1
+
+def test_dummu3():
+    assert 1 == 1
+
+def test_dummu5():
+    assert 1 == 1
+
+def test_dummu6():
+    assert 1 == 0
+
+def test_dummu7():
+    assert 1 == 1
